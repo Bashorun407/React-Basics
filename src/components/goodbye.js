24be688c4cba475ec5e12dd564cc7024df78d929
Bashorun Olusheyi <boboladvat@gmail.com>
2025-03-08 09:06:30 +0100

    Committing basic react changes to local repo

---
+++ src/components/goodbye.js
@@ -0,0 +1,7 @@
+// function GoodBye() {
+//     return <h1>Good bye friend, Godspeed!!</h1>
+// }
+
+const GoodBye = (props) => <h1>Good bye {props.name}, Godspeed!!</h1>
+
+export default GoodBye
---
+++ src/components/welcome.js
@@ -0,0 +1,8 @@
+// function Welcome() {
+//     return <h1>Welcome, Bashorun</h1>
+// }
+
+const Welcome = (props) => <h1>Welcome, {props.name}</h1>
+
+
+export default Welcome
