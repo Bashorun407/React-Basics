487211c7cd20bed2541cb2936f49d126e8c1f3db
Bashorun Olusheyi <boboladvat@gmail.com>
2025-03-10 19:49:45 +0100

    Updated User file with useState function that receives object as argument

--- src/components/user.js
+++ src/components/user.js
@@ -2,21 +2,37 @@ import { useState } from "react"
 
 
 const User = () => {
-    const [firstName, setFirstName] = useState('Olushile')
-    const [lastName, setLastName] = useState('Ajaguna')
-    const [email, setEmail] = useState('olush@gmail.com')
+    //Using useState function with empty-string as argument
+    // const [firstName, setFirstName] = useState('Olushile')
+    // const [lastName, setLastName] = useState('Ajaguna')
+    // const [email, setEmail] = useState('olush@gmail.com')
+
+    //Using useState function with object as argument
+    const [user, setUser] = useState({
+        firstName: 'Bolaji',
+        lastName: 'Ayobami',
+        email: 'bolayomi@gmail.com'
+    })
 
     function UpdateUser() {
-        setFirstName('Oluseye')
-        setLastName('Babade')
-        setEmail('babadana@gmail.com')
+        //These functions use useState function with string argument
+        // setFirstName('Oluseye')
+        // setLastName('Babade')
+        // setEmail('babadana@gmail.com')
+
+        //This useState function uses object argument
+        setUser({
+            firstName: 'Aboyeji',
+            lastName: 'Salami',
+            email: 'boyesalms@gmail.com'
+        })
     }
     return (
         <div>
             <h1>User Details</h1>
-            <p>{firstName}</p>
-            <p>{lastName}</p>
-            <p>{email}</p>
+            <p>{user.firstName}</p>
+            <p>{user.lastName}</p>
+            <p>{user.email}</p>
             <button onClick={UpdateUser}>Update User</button>
         </div>
     )
