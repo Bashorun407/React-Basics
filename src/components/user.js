9bdcf43d5230ec01a27dcac1d941dc9a670ab893
Bashorun Olusheyi <boboladvat@gmail.com>
2025-03-10 19:37:38 +0100

    Updated User file contains UpdateUser function

--- src/components/user.js
+++ src/components/user.js
@@ -6,12 +6,18 @@ const User = () => {
     const [lastName, setLastName] = useState('Ajaguna')
     const [email, setEmail] = useState('olush@gmail.com')
 
+    function UpdateUser() {
+        setFirstName('Oluseye')
+        setLastName('Babade')
+        setEmail('babadana@gmail.com')
+    }
     return (
         <div>
             <h1>User Details</h1>
             <p>{firstName}</p>
             <p>{lastName}</p>
             <p>{email}</p>
+            <button onClick={UpdateUser}>Update User</button>
         </div>
     )
 }
